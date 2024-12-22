2a85d60affcb0cfc01634be15285d59de27efacb
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2024-12-22 16:00:34 +0300

    about,contact

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,8 @@ import NewsletterBox from "../components/NewsletterBox.jsx";
 const About = () => {
     return (
         <div>
-            <div className={"text-2x1 text-center pt-8 border-t"}>
-                <Title text1={"ABOUT"} text2={"US"} />
+            <div className={"text-2xl text-center pt-8 border-t"}>
+                <Title primaryText={"ABOUT"} secondaryText={"US"} />
             </div>
             <div className={"my-10 flex flex-col md:flex-row gap-16"}>
                 <img className={"w-full md:max-w-[450px]"} src={assets.about_img} alt=""/>
@@ -17,19 +17,19 @@ const About = () => {
                     <p>Our mission at Ursamajor is to empower customers with choice, convenience and confidence. We are dedicated to providing a seamless shopping experience that exceeds expectations from browsing and ordering to delivery and beyond.</p>
                 </div>
             </div>
-            <div className={"text-4xl py-4"}>
-                <Title text1={"WHY"} text2={"CHOOSE US"} />
+            <div className={"text-2xl py-4"}>
+                <Title primaryText={"WHY"} secondaryText={"CHOOSE US"} />
             </div>
             <div className={"flex flex-col md:flex-row text-sm mb-20"}>
-                <div className={"border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap 5"}>
+                <div className={"border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5"}>
                     <b>Quality Assurance:</b>
                     <p className={"text-gray-600"}>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
                 </div>
-                <div className={"border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap 5"}>
+                <div className={"border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5"}>
                     <b>Convenience:</b>
                     <p className={"text-gray-600"}>With our user-friendly interface and hassle-free ordering-process, shopping has never been easier</p>
                 </div>
-                <div className={"border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap 5"}>
+                <div className={"border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5"}>
                     <b>Exceptional Customer Service:</b>
                     <p className={"text-gray-600"}>Our team of dedicated professionals is here to assist you the way,ensuring your satisfaction is top priority.</p>
                 </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,9 +6,9 @@ const Contact = () => {
     return (
         <div>
             <div className={"text-center text-2xl pt-10 border-t"}>
-                <Title text1={'Contact'} text2={'US'} />
+                <Title primaryText={'Contact'} secondaryText={'US'} />
             </div>
-            <div className={"my-10 flex -flx-col justify-center md:fle-row gap-10 mb-28"}>
+            <div className={"my-10 flex flex-col justify-center md:flex-row gap-10 mb-28"}>
                 <img className={"w-full md:max-w-[480px]"} src={assets.contact_img} alt=""/>
                 <div className={"flex flex-col justify-center item-start gap-6"}>
                     <p className={"font-semibold text-xl text-gray-600"}>Our Store</p>
