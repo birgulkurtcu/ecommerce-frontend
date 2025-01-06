75016b5c893f0a0035326d417eb7ea8feeffa752
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-01-07 01:12:38 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,30 @@ import Orders from "./pages/Orders.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import SearchBar from "./components/SearchBar.jsx";
-import { ToastContainer, toast } from 'react-toastify';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Verify from "./pages/Verify.jsx";
 
 const App = () => {
-  return (
-      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-        <ToastContainer />
-        <Navbar />
-        <SearchBar />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/collection" element={<Collection/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/product/:productId" element={<Product/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/place-order" element={<PlaceOrder/>} />
-          <Route path="/orders" element={<Orders/>} />
-        </Routes>
-        <Footer />
-      </div>
-  );
+    return (
+        <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+            <ToastContainer/>
+            <Navbar/>
+            <SearchBar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/collection" element={<Collection/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/product/:productId" element={<Product/>}/>
+                <Route path="/cart" element={<Cart/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/place-order" element={<PlaceOrder/>}/>
+                <Route path="/orders" element={<Orders/>}/>
+                <Route path="/verify" element={<Verify/>}/>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 };
 export default App;
